2f2936dc3e985e75dfa9e22b3052f46a69038356
salahmak <salah.mak@protonmail.com>
2025-02-24 18:30:48 +0100

    merge with main

--- lib/src/weekly_event.dart
+++ lib/src/weekly_event.dart
@@ -9,12 +9,20 @@ class WeeklyEvent<T> {
   final WeekDays weekday;
   final TimeOfDay startTime;
   final TimeOfDay endTime;
+  final Color color;
+  final String title;
   final T event;
+
+  /// Define style of title.
+  final TextStyle? titleStyle;
 
   WeeklyEvent({
     required this.weekday,
     required this.startTime,
     required this.endTime,
+    this.color = Colors.blue,
+    required this.title,
+    this.titleStyle,
     required this.event,
   });
 
@@ -30,7 +38,9 @@ class WeeklyEvent<T> {
       endTime: DateTime(
           date.year, date.month, date.day, endTime.hour, endTime.minute),
       event: event,
-      title: '',
+      title: title,
+      color: color,
+      titleStyle: titleStyle,
       // recurrenceSettings: RecurrenceSettings(
       //   frequency: RepeatFrequency.weekly,
       //   startDate: date,
